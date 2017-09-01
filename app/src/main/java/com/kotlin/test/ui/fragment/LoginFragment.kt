051400e56e805f3ec462507617b0a78753458c09
alvin <494673006@qq.com>
2017-09-01 17:46:16 +0800

    2017年9月1日添加新的东西

--- app/src/main/java/com/kotlin/test/ui/fragment/LoginFragment.kt
+++ app/src/main/java/com/kotlin/test/ui/fragment/LoginFragment.kt
@@ -1,9 +1,21 @@
 package com.kotlin.test.ui.fragment
 
+import android.util.Log
 import android.view.View
+import com.google.gson.Gson
 import com.kotlin.test.R
+import com.kotlin.test.http.HttpClient
 import com.kotlin.test.ui.activity.BaseActivity
+import com.kotlin.test.util.RsaUtil
+import com.kotlin.test.widget.ClearEditText
+import com.tencent.bugly.beta.global.e
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.fragment_login.*
+import okhttp3.MediaType
+import okhttp3.RequestBody
+import okhttp3.Response
+import retrofit2.HttpException
 
 /**
  * @Title LoginFragment
@@ -23,6 +35,7 @@ class LoginFragment : BaseFragment() {
 
     override fun initData() {
         id_tv_register.setOnClickListener(this)
+        id_btn_login.setOnClickListener(this)
     }
 
     override fun onClick(v: View) {
@@ -30,8 +43,43 @@ class LoginFragment : BaseFragment() {
             R.id.id_tv_register -> {
                 (context as BaseActivity).addFragment(R.id.id_container, RegisterFragment())
             }
+            R.id.id_btn_login -> {
+                initLogin()
+            }
             else -> {
+
             }
         }
+    }
+
+    fun initLogin() {
+        var account = findId<ClearEditText>(R.id.id_et_account)
+        var password = findId<ClearEditText>(R.id.id_et_password)
+        if (account.text.isEmpty()) {
+            account.error = "用户名不能为空"
+            account.requestFocus()
+            return
+        }
+        if (password.text.isEmpty()) {
+            password.error = "密码不能为空"
+            password.requestFocus()
+            return
+        }
+        var map = HashMap<String, String>()
+        map.put("account", account.text.toString())
+        map.put("password", password.text.toString())
+        Log.i("测试", RsaUtil.encryptWithRSA(Gson().toJson(map)))
+        var body: RequestBody = RequestBody.create(MediaType.parse("application/json;charset=UTF-8"), Gson().toJson(map))
+        HttpClient().mApi.register(body).subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread()).subscribe({
+            t ->
+            toast(t)
+            Log.i("测试", t)
+        }, { t ->
+            t.printStackTrace()
+            var e: HttpException = t as HttpException
+
+            Log.i("测试", e.response().errorBody()!!.string())
+
+        })
     }
 }
--- app/src/main/java/com/kotlin/test/util/RsaUtil.kt
+++ app/src/main/java/com/kotlin/test/util/RsaUtil.kt
@@ -2,13 +2,15 @@ package com.kotlin.test.util
 
 import android.util.Base64
 import java.security.KeyFactory
+import java.security.KeyFactorySpi
 import java.security.interfaces.RSAPublicKey
+import java.security.spec.PKCS8EncodedKeySpec
 import java.security.spec.X509EncodedKeySpec
 import javax.crypto.Cipher
 
 /**
  * @Title RsaUtil
- * @Description:
+ * @Description:Rsa 算法公钥加密
  * @Author: alvin
  * @Date: 2017/8/31.17:08
  * @E-mail: 49467306@qq.com
@@ -17,26 +19,23 @@ class RsaUtil {
 
 
     companion object {
-        private var mRSAPublicKey: RSAPublicKey? = null
-        fun loadPublicKey(pubkey: String) {
+        private val PUBLIC_KEY: String = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA8DBx61vl/L2+3ixl1KR6" +
+                "eepdFl5gqOyL2Y+M8PmHkKWUXjhcAUaYmFZ//llIBYlrahh1LpDrObA9vRoEEtpx" +
+                "PB65xE6oJgNM+kJOItRVpIwzZ76oLwWg0VY5/G2hTPoSclMAZF7UvKZebE0nH0V4" +
+                "uFqve7JrKwBGBOtjMEnvuxNspkVD9nZbmthm41kwMyfjN2cOiqw0DAVKxkRtu3zw" +
+                "yFYne0qwHFdnNp4vLA1Yd1XKl324TODEP0zSt3frSw559b2l8lHpSP0MxJx4Iobm" +
+                "1AU2OyQ8REXGBKFMBe869XV0czVZFSAA7hHW2KywulDZQWmJYFsnSxV7U2TLgg0C" +
+                "vwIDAQAB"
 
-            try {
-                var buffer = Base64.decode(pubkey, Base64.DEFAULT)
-                var keyFactory = KeyFactory.getInstance("RSA")
-                var keySpec = X509EncodedKeySpec(buffer)
-                mRSAPublicKey = keyFactory.generatePublic(keySpec) as RSAPublicKey
-            } catch(e: Exception) {
-                e.printStackTrace()
-            }
-
-        }
 
         fun encryptWithRSA(data: String): String {
-            if (mRSAPublicKey == null)
-                throw NullPointerException("encrypt PublicKey is null !")
+            var buffer = Base64.decode(PUBLIC_KEY.toByteArray(Charsets.UTF_8), Base64.DEFAULT)
+            var keySpec = X509EncodedKeySpec(buffer)
+            var keyFactory = KeyFactory.getInstance("RSA")
+            var publicKey = keyFactory.generatePublic(keySpec) as RSAPublicKey
             var cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding")
-            cipher.init(Cipher.ENCRYPT_MODE, mRSAPublicKey)
-            var output = cipher.doFinal(data.toByteArray(Charsets.UTF_8))
+            cipher.init(Cipher.ENCRYPT_MODE, publicKey)
+            var output = cipher.doFinal(Base64.decode(data.toByteArray(Charsets.UTF_8), Base64.DEFAULT))
             var encode = Base64.encode(output, Base64.DEFAULT)
             return Base64.encodeToString(encode, Base64.DEFAULT)
 
